94da3e6eccbe0b9a8bd5bf9b374e796bcc8cd92e
nurmdrafi <nurmdrafi2014@gmail.com>
2026-01-25 23:46:45 +0600

    test: husky
    Signed-off-by: nurmdrafi <nurmdrafi2014@gmail.com>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ function App() {
         <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'></div>
       </header>
       <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
-        <p className='text-gray-600'>test</p>
+        <p className='text-gray-600'>test husky</p>
       </main>
     </div>
   )
