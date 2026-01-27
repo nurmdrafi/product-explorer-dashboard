22bec2daffe8413be5cf8e8e24bfa49901ee9c64
nurmdrafi <nurmdrafi2014@gmail.com>
2026-01-28 01:36:15 +0600

    style: re-design settings page
    Signed-off-by: nurmdrafi <nurmdrafi2014@gmail.com>

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -3,12 +3,11 @@ import { CurrencySelector } from '@features/settings'
 export function SettingsPage() {
   return (
     <div className='container'>
+      <h1 className='text-3xl font-bold mb-8'>Settings</h1>
+      <div className='grid md:grid-cols-1 gap-8'>
       <div className='max-w-2xl mx-auto'>
-        <h1 className='text-3xl font-bold mb-8'>Settings</h1>
-
-        <div className='space-y-8'>
           <CurrencySelector />
-        </div>
+      </div>
       </div>
     </div>
   )
